6bc5ba04b3bcc872ca529302229b3d18d5797298
Lev <leva.belousov@yandex.ru>
2020-12-12 14:14:35 +0300

    feat: add axios

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -1,3 +1,27 @@
 <template>
     <p>menuuu</p>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            menu:{}
+        }
+    },
+    created(){
+        this.init()
+    },
+    methods: {
+        async init() {
+
+            const res = await this.$axios.get('/menu')
+            this.menu = res.data
+            console.log('we are in init function')
+        }
+    }
+
+}
+</script>
