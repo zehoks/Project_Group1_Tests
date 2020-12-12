c87c337a77c767546e86c12ffaa3abbeb1246f43
stasyavetch <vetchinovaanastasia@gmail.com>
2020-12-12 11:09:54 +0300

    feat: add api theme(continue)

--- Backend/index.js
+++ Backend/index.js
@@ -9,12 +9,25 @@ var cors = require('cors')
 
 const generationServices = require('./services/generation')
 const resultServices = require('./services/result')
+const themeServices = require('./services/test_theme')
 
 const app = express()
 // чтобы парсить application/json
 app.use(bodyParser.json())
 app.use(cors())
 
+
+app.route('/theme').get(async (res) => {
+    
+    try {
+        const theme = await themeServices.theme_description()
+        res.send(theme)
+    } catch (err) {
+        res.status(500).send({
+            error: err.message
+        })
+    }
+})
 
 app.listen(80, () => {
     console.log('Server started on http://localhost:80')
