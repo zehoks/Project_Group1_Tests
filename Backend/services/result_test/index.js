549d098759323d084d52c7a92fc74c213e635ce7
stasyavetch <vetchinovaanastasia@gmail.com>
2021-01-04 23:59:31 +0300

    fix: change api get result

--- Backend/services/result_test/index.js
+++ Backend/services/result_test/index.js
@@ -6,28 +6,26 @@ const pool = require('../../config/db')
  * @param {Number[number[], number[]]} answer_for_check - массив массивов (id вопросов, id ответов)
  */
 async function get_result(answer_for_check) {
-	const { rows: true_answer } = await pool.query(`
-    SELECT id, question_id, result_ 
-    FROM answer_question
-    WHERE result_ = $1 AND question_id in ($${answer_for_check[0]})
-    `)
-
 	let count_true_answer = 0
-	//let count_question = 0
-	for (const i of answer_for_check) {
-		//count_question++
-		for (const j of true_answer) {
-			if (j.question_id == i.id) {
-				if (i.id_answer == j.result_) {
-					count_true_answer++
-				}
-			}
-		}
-	}
-	//let res = {count_true_answer, count_question}
+	let count_question = 0
+	for (let i = 0; i < answer_for_check.length; i++) {
+		const { rows: true_answer } = await pool.query(
+			`
+        SELECT id, question_id, result_ 
+        FROM answer_question
+        WHERE result_ = 1 AND question_id = $1`,
+			[answer_for_check[i].id_question]
+		)
 
-	//return res
-	return count_true_answer
+		if (answer_for_check[i].id_answer == true_answer[i].id) {
+			count_true_answer++
+		}
+		count_question++
+	}
+
+	let res = (count_true_answer / count_question) * 100
+
+	return res
 }
 
 module.exports = {
